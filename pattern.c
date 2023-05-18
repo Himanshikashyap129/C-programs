1ea089969b0a92b24d612e0a661c6bab8689a8e2
Himanshi Kashyap <124158878+Himanshikashyap129@users.noreply.github.com>
2023-05-18 16:38:59 +0530

    Pattern.c

--- pattern.c
+++ pattern.c
@@ -1,16 +1,4 @@
 #include<stdio.h>
-// int main(){
-//     int i, j, k=1, n;
-//     printf("\nEnter the number of lines:");
-//     scanf("%d",&n);
-//     for(i=1; i<=n; i++){
-//         for(j=1; j<=i; j++, k++){
-//             printf("%4c",(char)(k+96));
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
 # include <stdio.h>
 # include <string.h>
 int main()
